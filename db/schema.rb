0e87f30d0273dfd14e2cb9bdba47d5a2caa8998d
Rafael Evangelista <rafael.evans@live.com>
2018-09-28 16:16:49 -0300

    [WiP] - Recipes by cuisine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_200328) do
+ActiveRecord::Schema.define(version: 2018_09_28_191034) do
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
